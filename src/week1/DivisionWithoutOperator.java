c2a0c7a8a597a1b959dab3bf528e21de0dd90ff2
Aizat Cervantes <aizamosk@yahoo.com>
2023-05-19 12:38:30 -0400

    Question 2 and 3 added

---
+++ src/week1/DivisionWithoutOperator.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class DivisionWithoutOperator {
+    /*
+
+ Numbers - Divide without / operator:
+Write a method that can divide two numbers without using division operator
+     */
+}
