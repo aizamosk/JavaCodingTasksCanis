4886573c493e8fb4fb819c49e354116912199567
aaronqa <aaron@cybertekschool.com>
2023-05-25 23:39:46 +0300

    week1

---
+++ src/Aaron/DivisionWithoutOperator.java
@@ -0,0 +1,30 @@
+package Aaron;
+
+public class DivisionWithoutOperator {
+    /*
+
+ Numbers - Divide without / operator:
+Write a method that can divide two numbers without using division operator
+     */
+    public static void main(String[] args) {
+        //Write a method that can divide two numbers without using division operator
+        divides(6,3);
+        divides(4,2);
+        divides(3,0);
+        divides(0,2);
+    }
+
+    public static void divides(int num1, int num2) {
+        if(num2==0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+        System.out.print(num1 +" divided by "+num2 +" is: ");
+        int count =0;
+        while(num1 >= num2) {
+            num1 -= num2;
+            count++;
+        }
+        System.out.println(count+" and remainder is "+num1);
+    }
+}
