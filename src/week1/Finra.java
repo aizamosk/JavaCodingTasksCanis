f700946b0aeaa837abd68aa517fc766414157496
kolotei <kolotei@yahoo.com>
2023-05-25 20:43:21 -0400

    Adding there solution to my remote Branch

--- src/week1/Finra.java
+++ src/week1/Finra.java
@@ -1,8 +1,23 @@
 package week1;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Finra {
     /*
     FINRA:
-Write a function which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3, print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number. For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+    Write a function which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3, print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number. For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
      */
+
+    public static void FINRA (){
+
+        for (int i = 1; i <= 30; i++) {
+            if (i >= 3 && i % 3 == 0 && i % 5 != 0) System.out.print("FIN ");
+            else if (i >= 5 && i % 3 != 0 && i % 5 == 0) System.out.print("RA ");
+            else if (i >= 5 && i % 3 == 0 && i % 5 == 0) System.out.print("FINRA ");
+            else System.out.print(i + " ");
+        }
+    }
+    public static void main(String[] args) {
+        FINRA();
+    }
 }
--- src/week1/OddOrEven.java
+++ src/week1/OddOrEven.java
@@ -1,5 +1,8 @@
 package week1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class OddOrEven {
     /*
 
@@ -10,5 +13,25 @@ identify(5); ->"Odd"
 identify(6); ->"Even"
 
      */
+
+    public static void Identifier(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter number to be identify");
+        int number = scan.nextInt();
+
+        if (number%2 != 0) {                        //checking if number entered has left over from division by 2
+            System.out.println("Odd");
+        } else System.out.println("Even");
+
+        scan.close();   //closing scannera
+    }
+    public static void main(String[] args) {
+        try {
+            Identifier();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("You`ve entered wrong type in console for identification, please enter numbers only!");
+        }
+    }
 
 }
