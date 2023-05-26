098a651ebde715eef83ec9619f853030f86ece7b
kolotei <kolotei@yahoo.com>
2023-05-25 20:58:07 -0400

    Adding there solution to my remote Branch

---
+++ src/Alexey/DivisionWithoutOperator.java
@@ -0,0 +1,60 @@
+package Alexey;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivisionWithoutOperator {
+    /*
+
+ Numbers - Divide without / operator:
+Write a method that can divide two numbers without using division operator
+     */
+
+    public static double Division(){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter number you want to be numerator");
+        double numerator = scan.nextDouble();
+        scan.nextLine();
+        int zeroPoint = 1;
+        if (numerator == 0){
+            return 0;
+        }
+        if (numerator < 0 ) {
+            zeroPoint *= -1;
+            numerator *= zeroPoint;
+        }
+
+        System.out.println("Please Enter number you want to be a denominator");
+        double denominator = scan.nextDouble();
+        scan.nextLine();
+        if (denominator == 0) {
+            throw new ArithmeticException ("you can`t divide numbers using 0 as denominator");
+        }
+        if (denominator < 0 ) {
+            zeroPoint *= -1;
+            denominator *= zeroPoint;
+        }
+        return  (zeroPoint * Division(numerator, denominator));
+        }
+
+    public static double Division(double numerator, double denominator){
+
+        int i = 0;
+        while (numerator >= denominator) {
+            numerator -= denominator;
+            ++i;
+        }
+        return ((double) i);
+    }
+
+        // scan.close();
+    public static void main(String[] args) {
+        try {
+            System.out.println("Results of the division are: " + Division());
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Please use numbers only!");
+        }
+    }
+}
