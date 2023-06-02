877fbb50758ad63ec000e7ebe443dbc591e0e3cf
aaronqa <aaron@cybertekschool.com>
2023-06-02 16:48:27 +0300

    week1

--- src/week3/Numbers_Reverse_Negative_Number.java
+++
@@ -1,5 +0,0 @@
-package week3;
-//Write a return method that can reverse digits of a negative number and return it as int
-//input: -35  output: -53
-public class Numbers_Reverse_Negative_Number {
-}
