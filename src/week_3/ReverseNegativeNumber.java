0cd0f92bf949f34c2f86d9ad81d4f3ccf4b314b8
Aizat Cervantes <aizamosk@yahoo.com>
2023-06-02 09:42:14 -0400

    week03 tasks

---
+++ src/week_3/ReverseNegativeNumber.java
@@ -0,0 +1,8 @@
+package week_3;
+
+public class ReverseNegativeNumber {
+    /*
+    Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+     */
+}
