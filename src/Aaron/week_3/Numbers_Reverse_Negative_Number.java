cc42d553d00509f8b2ffee1c8f424f3c758f6181
aaronqa <aaron@cybertekschool.com>
2023-06-02 16:06:06 +0300

    week1

---
+++ src/Aaron/week_3/Numbers_Reverse_Negative_Number.java
@@ -0,0 +1,29 @@
+package Aaron.week_3;
+//Write a return method that can reverse digits of a negative number and return it as int
+//input: -35  output: -53
+public class Numbers_Reverse_Negative_Number {
+    public static void main(String[] args) {
+        System.out.println(reverse(-35));
+        System.out.println(reverseNum(-53));
+
+    }
+    public static int reverse(int number) {
+        if (number < 10 && number >= 0) {
+            return number;
+        }
+        int result = 0;
+        while (number != 0) {
+            result = result * 10 + number % 10;
+            number /= 10;
+        }
+        return result;
+    }
+
+    public static int reverseNum(int num) {
+        String str = new StringBuilder("" + num).reverse().toString();
+        if (num < 0) {
+            str = "-" + str.substring(0, str.length() - 1);
+        }
+        return Integer.valueOf(str);
+    }
+}
---
+++ src/week3/NumbersPrimeNumber.java
@@ -0,0 +1,8 @@
+package week3;
+
+/**
+ *  Numbers -- PrimeNumber
+ * Write a method that can check if a number is prime or not
+ */
+public class NumbersPrimeNumber {
+}
