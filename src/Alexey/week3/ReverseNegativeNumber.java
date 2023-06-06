9d27919e290cd2ea55870e4a5a9ea1e4fe870cb3
kolotei <kolotei@yahoo.com>
2023-06-05 20:03:23 -0400

    Reorganized my files

---
+++ src/Alexey/week3/ReverseNegativeNumber.java
@@ -0,0 +1,11 @@
+package Alexey.week3;
+
+public class ReverseNegativeNumber {
+    /*
+    Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+     */
+
+
+
+}
--- src/week1/OddOrEven.java
+++ src/week1/OddOrEven.java
@@ -4,18 +4,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OddOrEven {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int num = 0;
 
-        if(num % 2 == 0) {
+        if (num % 2 == 0) {
             System.out.println("Even");
-        }else {
+        } else {
             System.out.println("Odd");
         }
 
     }
-
+}
     /*
 
  Numbers-Odd or even:
@@ -26,28 +26,3 @@ identify(6); ->"Even"
 
      */
     //wonderfullll
-
-
-
-
-    public static void Identifier(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please Enter number to be identify");
-        int number = scan.nextInt();
-
-        if (number%2 != 0) {                        //checking if number entered has left over from division by 2
-            System.out.println("Odd");
-        } else System.out.println("Even");
-
-        scan.close();   //closing scannera
-    }
-    public static void main(String[] args) {
-        try {
-            Identifier();
-        }
-        catch (InputMismatchException e) {
-            System.out.println("You`ve entered wrong type in console for identification, please enter numbers only!");
-        }
-    }
-
-}
