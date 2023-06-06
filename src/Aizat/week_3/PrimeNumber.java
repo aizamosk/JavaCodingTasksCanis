03a38cf55e70d4be56584197abd23a5673b39dd9
Aizat Cervantes <aizamosk@yahoo.com>
2023-06-06 12:44:58 -0400

    week03 tasks completed

--- src/Aizat/week_3/PrimeNumber.java
+++ src/Aizat/week_3/PrimeNumber.java
@@ -4,4 +4,26 @@ public class PrimeNumber {
     /*
     Write a method that can check if a number is prime or not.
      */
+
+    public static boolean isPrimeNumber(int n) { //is my method
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            return false;
+
+        }
+
+      return true;
+
+
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=100 ; i++) {
+            if(isPrimeNumber(i));{   // I  am calling my method
+                System.out.println(i +" this number is a prime number ");
+            }
+        }
+    }
 }
