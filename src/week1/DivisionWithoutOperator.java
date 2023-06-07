d04f881291e99e7dab112c866e96be1d1b199e51
aaronqa <aaron@cybertekschool.com>
2023-06-07 19:51:01 +0300

    week1

--- src/week1/DivisionWithoutOperator.java
+++ src/week1/DivisionWithoutOperator.java
@@ -1,12 +1,7 @@
 package week1;
-
-
 public class DivisionWithoutOperator {
     /*
-
- Numbers - Divide without / operator:
+Numbers - Divide without / operator:
 Write a method that can divide two numbers without using division operator
      */
-
-
 }
--- src/week1/OddOrEven.java
+++ src/week1/OddOrEven.java
@@ -1,24 +1,11 @@
 package week1;
 
-
-
 public class OddOrEven {
-
-
     /*
-
- Numbers-Odd or even:
+Numbers-Odd or even:
 Write  a method which can identify given number is even or odd
 Output ex:
 identify(5); ->"Odd"
 identify(6); ->"Even"
-
      */
-    //wonderfullll
-
-
-
-
-
-
 }
