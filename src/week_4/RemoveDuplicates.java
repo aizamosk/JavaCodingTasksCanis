7f5f2b64c1d3be3eed7ff3cc5144a1f52153bea7
Aizat Cervantes <aizamosk@yahoo.com>
2023-06-08 11:13:07 -0400

    week04 task requirements

---
+++ src/week_4/RemoveDuplicates.java
@@ -0,0 +1,9 @@
+package week_4;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+}
