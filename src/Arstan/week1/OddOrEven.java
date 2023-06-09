2e3e1a3df2301868ff13b4142e38c5f3a0972894
arstan <arstankmn@gmail.com>
2023-06-09 16:25:02 -0500

    week 2 solutions

---
+++ src/Arstan/week1/OddOrEven.java
@@ -0,0 +1,29 @@
+package Arstan.week1;
+
+public class OddOrEven {
+    /*
+
+ Numbers-  Odd or even:
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"
+
+     */
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+    }
+
+    public static void OddOrEven(int a) {
+
+
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
+
+
