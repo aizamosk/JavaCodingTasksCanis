a4090299fce746df23bab826d9cfd82869af6cbe
Yavuz <albayrak.yavuz@yahoo.com>
2023-06-25 23:29:13 -0400

    solved week 5 find unique character task

--- src/week_5/UniqueCharacters.java
+++
@@ -1,8 +0,0 @@
-package week_5;
-
-public class UniqueCharacters {
-    /*
-    Write a return  method that can find the unique characters from the String
-    Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
-     */
-}
