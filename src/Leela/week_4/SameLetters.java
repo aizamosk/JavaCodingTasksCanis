13acfe5fb4a0056955d4978d2c30ce0352578af5
Leela Bhandari <123910619+Leela620@users.noreply.github.com>
2023-06-28 20:52:29 -0400

    week5 solutions

--- src/Leela/week_4/SameLetters.java
+++ src/Leela/week_4/SameLetters.java
@@ -1,7 +1,16 @@
 package Leela.week_4;
 
 public class SameLetters {
+
+
+
+
+
+    }
+
+
+
+
     /*Write a return method that check if a string is build out of the same letters as another string.
     Ex:  same("abc",  "cab"); -> true
     same("abc",  "abb"); -> false:*/
-}
