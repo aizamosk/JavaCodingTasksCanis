cd1df91f1dd370422f2970138a6d2f543c7d0ae9
Aizat Cervantes <aizamosk@yahoo.com>
2023-07-25 09:51:12 -0400

    week10 task requirements

---
+++ src/week_10/List_RemoveSomeValues.java
@@ -0,0 +1,7 @@
+package week_10;
+
+public class List_RemoveSomeValues {
+    /*
+    Given a list of Integers 1, 2, 3, 4, 5, 6, 101, 200, 300....etc. remove all values greater than 100.
+     */
+}
