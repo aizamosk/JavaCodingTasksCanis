c55415b2fcb1ef1f6cd1e5bec0120088f843e76d
odilj <odiljono@outlook.com>
2023-08-02 16:39:24 -0400

    Week 10 solutions

---
+++ src/Odil/week10/ArrayListSortDescending.java
@@ -0,0 +1,35 @@
+package Odil.week10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListSortDescending {
+    /*
+Write a method that can sort the ArrayList in Descending order without using the sort method.
+     */
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(5,2,9,1,5));
+
+        System.out.println("Before sorting: " + list);
+        arrayListSortDSC(list);
+        System.out.println("After sorting: " + list);
+    }
+
+    public static void arrayListSortDSC(ArrayList<Integer> list) {
+        int n = list.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) < list.get(j + 1)) {
+                    // Swap elements at j and j+1
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+}
