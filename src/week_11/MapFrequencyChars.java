6380524b1250e885d7a6d654765f87f2f60ff6b5
aaronqa <aaron@cybertekschool.com>
2023-08-14 20:42:51 +0300

    week 11 questions

---
+++ src/week_11/MapFrequencyChars.java
@@ -0,0 +1,8 @@
+package week_11;
+
+
+
+//Write a method that prints the frequency of each character from a String
+public class MapFrequencyChars {
+
+}
---
+++ src/week_11/mapMinValue.java
@@ -0,0 +1,8 @@
+package week_11;
+
+
+
+//Write a method that can return the minimum value from a map (DO NOT use sort method)
+public class mapMinValue {
+
+}
