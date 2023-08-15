957863173ad07ba334be072977d21e332121c764
Leela Bhandari <123910619+Leela620@users.noreply.github.com>
2023-08-15 17:45:21 -0400

    week 9 solutions

---
+++ src/Leela/week_9/List_RemoveNameAhmed.java
@@ -0,0 +1,21 @@
+package Leela.week_9;
+
+public class List_RemoveNameAhmed {
+
+    public static void removeAhmed(){
+
+    }
+    public static void main(String[] args) {
+
+        //input string = "Ahmed", "John", Eric", "Ahmed", "Aaron".....
+        //output string = "Ahmed", "John", Eric", "Aaron".....
+
+
+
+
+    }
+    /*
+    Given a list of people names: "Ahmed", "John", Eric", "Ahmed", "Aaron".....
+    Write a java operation to remove all the names named Ahmed
+     */
+}
